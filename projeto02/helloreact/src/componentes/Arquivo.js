60b7dc036e90e416f79c743fedec8215bd16bbeb
Eron Oliveira <eron.fisica@gmail.com>
2021-12-01 10:42:06 -0300

    Update arquivo.js

--- projeto02/helloreact/src/componentes/Arquivo.js
+++ projeto02/helloreact/src/componentes/Arquivo.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react"; //declara o state
 
 function Arquivo(props) {
-    let [num, setNum] = useState(10)
+    let [num, setNum] = useState(10) // declara o state: [nome, setNomeFunçãoQueVaiAlterar]=useState('valor inicial')
     return (
         <p>
             <p>Valor do state num: {num}</p>
             <button onClick={() => setNum(num + 10)}>Soma 10</button>
         </p>
-    )
+    )//chama a função para alterar o valor do state
+
 }
 export default Arquivo
