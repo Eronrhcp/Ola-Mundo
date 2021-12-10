687c1ef44671e67d46fa9d338a446c6573f52106
Eron Oliveira <eron.fisica@gmail.com>
2021-12-10 11:48:03 -0300

    Summary

--- projeto02/helloreact/src/App.js
+++ projeto02/helloreact/src/App.js
@@ -1,20 +1,11 @@
 import React, { useState } from "react"
 import './App.css'
-import Notas from "./componentes/Notas"
-import Resultado from "./componentes/Resultado"
 
 export default function App() {
-  let [nota1, setNota1] = useState(0)
-  let [nota2, setNota2] = useState(0)
-  let [nota3, setNota3] = useState(0)
-  let [nota4, setNota4] = useState(0)
+
   return (
     <div className='bg-white w-3/5 mx-auto p-5 m-5 border-2 rounded-md border-gray-400'>
-      <Notas num={1} nota={nota1} setNota={setNota1} />
-      <Notas num={2} nota={nota2} setNota={setNota2} />
-      <Notas num={3} nota={nota3} setNota={setNota3} />
-      <Notas num={4} nota={nota4} setNota={setNota4} />
-      <Resultado somaNotas={nota1 + nota2 + nota3 + nota4} />
+      <h1>Soma de Notas</h1>
     </div>
   )
 }
--- projeto02/helloreact/src/componentes/Notas.js
+++ projeto02/helloreact/src/componentes/Notas.js
@@ -4,9 +4,35 @@ export default function Notas(props) {
 
     return (
         <div>
-            <legend>Informe a nota:</legend>
-            <input type="text" value="" onChange=""></input>
+            <legend>Informe a nota: {props.num}</legend>
+            <input type="text" value={props.nota} onChange={(e) => props.setNota(e.target.value)}></input>
         </div>
     )
 
 }/* utilizar junto com resultado.js */
+
+/*
+
+import React, { useState } from "react"
+import './App.css'
+import Notas from "./componentes/Notas"
+import Resultado from "./componentes/Resultado"
+
+export default function App() {
+  let [nota1, setNota1] = useState(0)
+  let [nota2, setNota2] = useState(0)
+  let [nota3, setNota3] = useState(0)
+  let [nota4, setNota4] = useState(0)
+  return (
+    <div className='bg-white w-3/5 mx-auto p-5 m-5 border-2 rounded-md border-gray-400'>
+      <h1>Soma de Notas</h1>
+      <Notas num={1} nota={nota1} setNota={setNota1} />
+      <Notas num={2} nota={nota2} setNota={setNota2} />
+      <Notas num={3} nota={nota3} setNota={setNota3} />
+      <Notas num={4} nota={nota4} setNota={setNota4} />
+      <Resultado somaNotas={parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)} />
+    </div>
+  )
+}
+
+*/
