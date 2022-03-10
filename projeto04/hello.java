2f35c83aba05839e5da393ab00fe0727f0f75cdf
Eron Oliveira <eron.fisica@gmail.com>
2022-03-10 13:29:49 -0300

    Summary

---
+++ projeto04/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) { // void => retorna nada; main => método; array de strings chamado args
+        System.out.print("Oi Mundo!"); //imprime
+    }
+}
+
+// javac nomedoarquivo transforma o arquivo em um arquivo executácel para java
