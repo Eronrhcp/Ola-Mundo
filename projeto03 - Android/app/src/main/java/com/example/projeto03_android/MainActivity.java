5d520147701a4dd0df3bfae1aaa8c7e87d229e15
Eron Oliveira <eron.fisica@gmail.com>
2022-03-22 09:54:33 -0300

    Summary

--- projeto03 - Android/app/src/main/java/com/example/projeto03_android/MainActivity.java
+++ projeto03 - Android/app/src/main/java/com/example/projeto03_android/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.projeto03_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+        TextView tv = (TextView)findViewById(R.id.tv_nome);
+        EditText et_nome = (EditText) findViewById(R.id.et_nome);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(et_nome.getText());
+            }
+        });
     }
 }
